69683a385bee1821710f33fb7791b56f60f665d7
GeralBot19 <166670136+GeralBot19@users.noreply.github.com>
2024-06-12 20:48:40 -0500

    a

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,7 +5,7 @@ throw false
 }
 let pesan = args.join` `
 let oi = `👨🏻‍💻💙➢ 𝐁𝐨𝐭 𝐁𝐨𝐭𝐜𝐢𝐭𝐨 𝐩𝐚𝐫𝐚 𝐮𝐬𝐭𝐞𝐝𝐞𝐬 𝐁𝐞𝐛𝐞𝐬:  ${pesan}`
-let teks = `Creador Wa.me/51988802645 SI QUIERES BOT COMUNICAME 
+let teks = `     
 👨🏻‍💻💙 𝐑𝐞𝐯𝐢𝐯𝐚𝐧 𝐁𝐨𝐭𝐬-𝐆𝐞𝐫𝐚𝐥 𝐁𝐨𝐭 💙👨🏻‍💻 \n\n ${oi}\n\n 👨🏻‍💻💙➢𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬: \n`
 for (let mem of participants) {
 teks += `👨🏻‍💻💙≽ @${mem.id.split('@')[0]}\n`}
